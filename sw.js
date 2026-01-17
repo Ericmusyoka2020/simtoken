b85ab581e38c8c741eb1364691b94133dc957777
eric musyoka <eric@kali.eric>
2026-01-17 19:38:38 +0300

    change

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'sim-toolkit-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // add '/send.html', '/lipa.html' etc. when you create them
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("sim-toolkit-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
